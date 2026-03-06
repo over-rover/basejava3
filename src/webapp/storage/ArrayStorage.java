c5609856d53dfc86ccc848ba1576f9780d71f6b6
Alex Pav <gh.pueblo@gmail.com>
2026-03-06 12:28:17 +0300

    hw_2: Подсмотрел у себя самого в прошлом курсе и вношу дополнительные изменения. 1. Метод getIfExist переименован в getSearchKey (для единообразия). 2. Изменена логика метода isExist. Сам не догадался передавать ему индекс в качестве параметра. 3. Рефакторинг по пунктам 1 и 2.

--- src/webapp/storage/ArrayStorage.java
+++ src/webapp/storage/ArrayStorage.java
@@ -14,8 +14,8 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        int index = getIndexIfExist(r.getUuid());
-        if (index >= 0)
+        int index = getSearchKey(r.getUuid());
+        if (isExist(index))
             storage[index] = r;
         else
             System.out.println("UPDATE ERROR: " + r.getUuid() + " не существует");
@@ -25,7 +25,8 @@ public class ArrayStorage {
         if (size == storage.length - 1)
             System.out.println("SAVE ERROR: " + r.getUuid() + " не добавлено. Хранилище заполнено");
 
-        if (!isExist(r)) {
+        int index = getSearchKey(r.getUuid());
+        if (!isExist(index)) {
             storage[size] = r;
             size++;
         } else
@@ -33,8 +34,8 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = getIndexIfExist(uuid);
-        if (index >= 0)
+        int index = getSearchKey(uuid);
+        if (isExist(index))
             return storage[index];
         else {
             System.out.println("GET ERROR: " + uuid + " не существует");
@@ -43,8 +44,8 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        int index = getIndexIfExist(uuid);
-        if (index >= 0) {
+        int index = getSearchKey(uuid);
+        if (isExist(index)) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
@@ -61,7 +62,7 @@ public class ArrayStorage {
         return size;
     }
 
-    private int getIndexIfExist(String uuid) {
+    private int getSearchKey(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid()))
                 return i;
@@ -69,7 +70,7 @@ public class ArrayStorage {
         return -1;
     }
 
-    private boolean isExist(Resume r) {
-        return getIndexIfExist(r.getUuid()) >= 0;
+    private boolean isExist(int index) {
+        return index >= 0;
     }
 }
