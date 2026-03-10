5e752d2e8dc05830f8a3e64ad3a0340611904abd
Alex Pav <gh.pueblo@gmail.com>
2026-03-10 13:17:03 +0300

    lesson_3: реализован интерфейс Comparable

--- src/webapp/model/Resume.java
+++ src/webapp/model/Resume.java
@@ -1,6 +1,6 @@
 package webapp.model;
 
-public class Resume {
+public class Resume implements Comparable<Resume> {
     private String uuid;
 
     public String getUuid() {
@@ -27,5 +27,10 @@ public class Resume {
     @Override
     public String toString() {
         return uuid;
+    }
+
+    @Override
+    public int compareTo(Resume o) {
+        return uuid.compareTo(o.uuid);
     }
 }
