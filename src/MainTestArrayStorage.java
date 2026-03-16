af9af8b3f0078538ab0f7bc0fe5335a85a4942ae
Alex Pav <gh.pueblo@gmail.com>
2026-03-16 14:15:55 +0300

    hw_4: Выполнен рефакторинг классов с main-методами для возможности компиляции проекта

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -11,12 +11,9 @@ public class MainTestArrayStorage {
     private static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
-        final Resume r1 = new Resume();
-        r1.setUuid("uuid1");
-        final Resume r2 = new Resume();
-        r2.setUuid("uuid3");
-        final Resume r3 = new Resume();
-        r3.setUuid("uuid2");
+        final Resume r1 = new Resume("uuid1");
+        final Resume r2 = new Resume("uuid2");
+        final Resume r3 = new Resume("uuid3");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
@@ -28,7 +25,6 @@ public class MainTestArrayStorage {
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         System.out.println("r1 before update: " + ARRAY_STORAGE.get(r1.getUuid()));
-        r1.setUuid("uuid0");
         ARRAY_STORAGE.update(r1);
         System.out.println("r1 after update: " + ARRAY_STORAGE.get(r1.getUuid()));
 
