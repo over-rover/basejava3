fa3a78f5d01fbd2acb4304fb023f2599c8e9c1bd
Alex Pav <gh.pueblo@gmail.com>
2026-03-16 13:59:10 +0300

    hw_4: Реализованы тестовые классы с использованием JUnit с последующим перемещением в пакет junit4

---
+++ test/webapp/storage/junit4/ArrayStorageTest.java
@@ -0,0 +1,10 @@
+package webapp.storage.junit4;
+
+import webapp.storage.ArrayStorage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
---
+++ test/webapp/storage/junit4/SortedArrayStorageTest.java
@@ -0,0 +1,10 @@
+package webapp.storage.junit4;
+
+import webapp.storage.SortedArrayStorage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
