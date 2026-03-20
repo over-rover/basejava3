d89840481f165059c23721c4dda9178062b3ab79
Alex Pav <gh.pueblo@gmail.com>
2026-03-20 11:23:19 +0300

    hw_4: произведена миграция тестов с JUnit 4 на JUnit 6; тестовые классы реализованы в пакете junit6

---
+++ test/webapp/storage/junit6/ArrayStorageTest.java
@@ -0,0 +1,10 @@
+package webapp.storage.junit6;
+
+import webapp.storage.ArrayStorage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
---
+++ test/webapp/storage/junit6/SortedArrayStorageTest.java
@@ -0,0 +1,10 @@
+package webapp.storage.junit6;
+
+import webapp.storage.SortedArrayStorage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
