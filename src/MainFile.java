39f184a15c731524e42fc958b2403cf186802c3b
Alex Pav <gh.pueblo@gmail.com>
2026-04-19 19:54:22 +0300

    hw_8: реализован рекурсивный обход src и ее подпапок

---
+++ src/MainFile.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+
+public class MainFile {
+    public static final StringBuilder INDENT = new StringBuilder();
+
+    public static void main(String[] args) {
+        String filePath = "src";
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+            System.out.println(file.getAbsolutePath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File("src");
+        System.out.println(dir.getName());
+        recurseWalk(dir);
+    }
+
+    public static void recurseWalk(File obj) {
+        if (obj.isFile()) return;
+        File[] objects = obj.listFiles();
+        if (objects != null) {
+            INDENT.append(" ");
+            for (File element : objects) {
+                System.out.println(INDENT + element.getName());
+                recurseWalk(element);
+            }
+            INDENT.deleteCharAt(INDENT.length() - 1);
+        }
+    }
+}
