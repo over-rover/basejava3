13fdc5251288cf442bc515e5922580226690774f
Alex Pav <gh.pueblo@gmail.com>
2026-05-04 09:36:25 +0300

    hw_9: В StorageException добавлены дополнительные конструкторы.

--- src/webapp/exception/StorageException.java
+++ src/webapp/exception/StorageException.java
@@ -3,9 +3,17 @@ package webapp.exception;
 public class StorageException extends RuntimeException {
     private final String uuid;
 
+    public StorageException(String message) {
+        this(message, "");
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
+    }
+
+    public StorageException(String message, Exception e) {
+        this(message, "", e);
     }
 
     public StorageException(String message, String uuid, Exception e) {
