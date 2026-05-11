40a197c2234078626be23a566d4dfef9a5b518de
Alex Pav <gh.pueblo@gmail.com>
2026-05-11 07:38:57 +0300

    hw_9: переименованы локальная переменная, параметр и метод. Выполнена проверка на null. Сокращен код метода compareFiles

--- src/webapp/util/MainFile.java
+++ src/webapp/util/MainFile.java
@@ -1,13 +1,12 @@
 package webapp.util;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Objects;
 
 public class MainFile {
-    public static final Comparator<File> FILE_COMPARATOR = MainFile::doCompare;
+    private static final Comparator<File> FILE_COMPARATOR = MainFile::compareFiles;
 
     public static void main(String[] args) {
         File dir = new File("src");
@@ -15,27 +14,25 @@ public class MainFile {
         recurseWalk(dir, "");
     }
 
-    private static void recurseWalk(File obj, String offset) {
-        Objects.requireNonNull(obj);
-        File[] objects = obj.listFiles();
-        Arrays.sort(objects, FILE_COMPARATOR);
-        for (File element : objects) {
-            if (element.isFile()) {
+    private static void recurseWalk(File dir, String offset) {
+        Objects.requireNonNull(dir, "directory must not be null");
+        File[] entries = dir.listFiles();
+        Objects.requireNonNull(entries, "directory is not found");
+        Arrays.sort(entries, FILE_COMPARATOR);
+        for (File entry : entries) {
+            if (entry.isFile()) {
                 String fileOffset = offset.substring(0, offset.length() - 2) + "|   ";
-                System.out.println(fileOffset + element.getName());
+                System.out.println(fileOffset + entry.getName());
             } else {
-                System.out.println(offset + "|_" + element.getName());
-                recurseWalk(element, offset + "  ");
+                System.out.println(offset + "|_" + entry.getName());
+                recurseWalk(entry, offset + "  ");
             }
         }
     }
 
-    private static int doCompare(File o1, File o2) {
-        if (o1.isDirectory() && o2.isFile()) {
-            return 1;
-        } else if (o1.isFile() && o2.isDirectory()) {
-            return -1;
-        }
+    private static int compareFiles(File o1, File o2) {
+        if (o1.isFile() && o2.isDirectory()) return -1;
+        if (o1.isDirectory() && o2.isFile()) return 1;
         return o1.compareTo(o2);
     }
 }
