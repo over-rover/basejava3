8823f16a06021bd07a7450bff9747b1d30e075ff
Alex Pav <gh.pueblo@gmail.com>
2026-06-22 17:52:06 +0300

    lesson_13: добавлен SQL-скрипт инициализации базы данных

---
+++ config/init_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE resume
+(
+    uuid      char(36) not null primary key ,
+    full_name text     not null
+);
+
+create table contact
+(
+    id          serial,
+    resume_uuid char(36) not null references resume (uuid) on delete cascade,
+    type        text     not null,
+    value       text     not null
+);
+
+create unique index contact_uuid_type_index
+    on contact (resume_uuid, type);
